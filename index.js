2f9bf354e9f45ac711a64e9ec54c9f44aa15bacd
Leonardo Klinger <gamefivebr@gmail.com>
2022-09-19 17:59:03 -0300

    make it better1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 require("./src/config/DB/Conexao.config").conectar()
 const { server } = require("./src/config/app")
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
     console.log(`Api ligada com sucesso na porta ${port}`)

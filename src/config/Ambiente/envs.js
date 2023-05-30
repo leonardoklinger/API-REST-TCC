bcceaea8f235824c70dd85216217370ed6dccc9c
Leonardo Klinger <gamefivebr@gmail.com>
2023-05-30 15:25:22 -0300

    vercel

--- src/config/Ambiente/envs.js
+++ src/config/Ambiente/envs.js
@@ -2,21 +2,17 @@ const config = {
     prod: {
         URL_MONGODB: process.env.URL_MONGODB,
         SECRET: process.env.SECRET,
-        SECRET_RESETAR_EMAIL: process.env.SECRET_RESETAR_EMAIL,
         HOST_EMAIL: process.env.HOST_EMAIL,
         EMAIL_EMAIL: process.env.EMAIL_EMAIL,
         SENHA_EMAIL: process.env.SENHA_EMAIL,
-        URL_FRONT_END: process.env.URL_FRONT_END,
         MONGODB_MENSAGEM: "MongoDB producao conectado com sucesso ✅"
     },
     dev: {
         URL_MONGODB: `mongodb://${process.env.DB_HOSTNAME}/${process.env.DB_PORT}`,
         SECRET: process.env.SECRET,
-        SECRET_RESETAR_EMAIL: process.env.SECRET_RESETAR_EMAIL,
         HOST_EMAIL: process.env.HOST_EMAIL,
         EMAIL_EMAIL: process.env.EMAIL_EMAIL,
         SENHA_EMAIL: process.env.SENHA_EMAIL,
-        URL_FRONT_END: process.env.URL_FRONT_END,
         MONGODB_MENSAGEM: "MongoDB desenvolvimento conectado com sucesso ✅"
     }
 }
